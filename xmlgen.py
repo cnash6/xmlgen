d70a4df3085c2332c82f0827e43c3b21658928d1
unknown <CNash@MALT-7191.us.manh.com>
2013-10-14 16:27:15 -0400

    10/14 update

--- xmlgen.py
+++ xmlgen.py
@@ -8,9 +8,15 @@ import tkinter.filedialog as tkfd
 import csv
 import xml.etree.ElementTree as et
 from time import gmtime, strftime
+import datetime
 import os
 from tkinter import ttk
 import time
+import shutil
+import threading
+from tkinter import tix
+import subprocess
+import math
 
 
 class GUI:
@@ -39,41 +45,44 @@ class GUI:
         XMLLabel.grid(row=4, column=0, columnspan=2)
 
 
-        browse = Button(f, text="Browse", command=self.openXMLFile)
-        browse.grid(row=5, column=0, pady=5, padx=5, sticky="e")
-
+        self.browse1 = Button(f, text="Browse", command=self.openXMLFile)
+        self.browse1.grid(row=5, column=0, pady=5, padx=5, sticky="e")
+        
+        self.XMLfname = ". . ."
         self.XMLEntry = Entry(f, width=50)
-        self.XMLEntry.insert(0,'. . .')
-        self.XMLEntry.config(state='readonly')
+        self.XMLEntry.insert(0,self.XMLfname)
+        #self.XMLEntry.config(state='readonly')
         self.XMLEntry.grid(row=5, column=1, pady=5, sticky="w")
 
         CSVLabel = Label(f, text="Select a CSV File", bg="#DBDBDB")
         CSVLabel.grid(row=6, column=0, columnspan=2)
 
-        browse = Button(f, text="Browse", command=self.openCSVFile)
-        browse.grid(row=7, column=0, pady=5, padx=5, sticky='e')
+        self.browse2 = Button(f, text="Browse", command=self.openCSVFile)
+        self.browse2.grid(row=7, column=0, pady=5, padx=5, sticky='e')
 
+        self.CSVfname = ". . ."
         self.CSVEntry = Entry(f, width=50)
-        self.CSVEntry.insert(0,'. . .')
-        self.CSVEntry.config(state='readonly')
+        self.CSVEntry.insert(0,self.CSVfname)
+        #self.CSVEntry.config(state='readonly')
         self.CSVEntry.grid(row=7, column=1, pady=5, sticky="w")
 
         DLabel = Label(f, text="Select File Destination", bg="#DBDBDB")
         DLabel.grid(row=8, column=0, columnspan=2)
 
-        browse = Button(f, text="Browse", command=self.getDirectory)
-        browse.grid(row=9, column=0, pady=5, padx=5, sticky='e')
+        self.browse3 = Button(f, text="Browse", command=self.getDirectory)
+        self.browse3.grid(row=9, column=0, pady=5, padx=5, sticky='e')
 
+        self.fDirectory = ". . ."
         self.DEntry = Entry(f, width=50)
-        self.DEntry.insert(0,'. . .')
-        self.DEntry.config(state='readonly')
+        self.DEntry.insert(0,self.fDirectory)
+        #self.DEntry.config(state='readonly')
         self.DEntry.grid(row=9, column=1, pady=5, sticky="w")
 
-        cont = Button(f, text="Continue", command=self.doIt)
-        cont.grid(row=10, pady=5, columnspan=2)
+        self.cont = Button(f, text="Continue", command=self.doIt)
+        self.cont.grid(row=10, pady=5, columnspan=2)
 
-        self.isWorking = False
-        test = Button(f, text="Test", command=self.progress)
+        self.isWorking = True
+        test = Button(f, text="Test", command=self.test)
         test.grid(row=11, pady=5, columnspan=2)
 
         menubar = Menu(root)
@@ -84,10 +93,12 @@ class GUI:
         helpmenu.add_command(label="About...", command=self.showHelp)
         menubar.add_cascade(label="Help", menu=helpmenu)
         root.config(menu=menubar)
+
+        self.datalen=0
 	
         self.rootwin.mainloop()
 
-    def showHelp(self):
+    def showHelp(self): ## Shows help box
         tkmb.showinfo("Help",
                       "XML Generator 1.0\nChristian Nash \n\n"
                       + "Instructions for use:\n\n"
@@ -101,11 +112,11 @@ class GUI:
         self.rootwin.destroy()
         
 
-    def openXMLFile(self): #this method simply opens a file dialog box when the select file button is clicked. it allows teh user to select a file with which he/she wishes to use. it puts teh file path in the file entry field and sets it to readonly so teh user can copy it but not manipulate it
+    def openXMLFile(self): #this method opens a file dialog box when the select file button is clicked. it allows teh user to select a file with which he/she wishes to use. it puts teh file path in the file entry field and sets it to readonly so teh user can copy it but not manipulate it
         self.XMLfname = tkfd.askopenfilename()
 
         try:
-            if self.XMLfname[len(self.XMLfname)-4:] != '.xml':
+            if len(self.XMLfname) < 5 or self.XMLfname[len(self.XMLfname)-4:] != '.xml':
                 raise ValueError
             XMLfile = open(self.XMLfname,'r')
             self.myTree = et.parse(XMLfile)
@@ -114,17 +125,17 @@ class GUI:
             self.filename = self.XMLfname[self.XMLfname.rfind('/')+1:len(self.XMLfname)-4]
         except:
             tkmb.showwarning("Error", "Error opening XML file.  Please verify file path and extension (.xml only)")
-            self.XMLEntry.config(state=NORMAL)
+            #self.XMLEntry.config(state=NORMAL)
             self.XMLEntry.delete(0,END)
             self.XMLEntry.insert(0,'. . .')
-            self.XMLEntry.config(state='readonly')
+            #self.XMLEntry.config(state='readonly')
             return
         
         if self.XMLfname:
-            self.XMLEntry.config(state=NORMAL)
+            #self.XMLEntry.config(state=NORMAL)
             self.XMLEntry.delete(0,END)
             self.XMLEntry.insert(0,self.XMLfname)
-            self.XMLEntry.config(state='readonly')
+            #self.XMLEntry.config(state='readonly')
 
         
 
@@ -133,7 +144,7 @@ class GUI:
         self.CSVfname = tkfd.askopenfilename()
 
         try:
-            if self.CSVfname[len(self.CSVfname)-4:] != '.csv':
+            if len(self.CSVfname) < 5 or self.CSVfname[len(self.CSVfname)-4:] != '.csv':
                 raise ValueError
             CSVFile = open(self.CSVfname, 'r')
             self.csvdata = CSVFile.readlines()
@@ -143,70 +154,183 @@ class GUI:
                 self.csvdata[x][len(self.csvdata[x])-1] = self.csvdata[x][len(self.csvdata[x])-1][:len(self.csvdata[x][len(self.csvdata[x])-1])-1]
         except:
             tkmb.showwarning("Error", "Error opening CSV file.  Please verify file path and extension (.csv only)")
-            self.CSVEntry.config(state=NORMAL)
+            #self.CSVEntry.config(state=NORMAL)
             self.CSVEntry.delete(0,END)
             self.CSVEntry.insert(0,'. . .')
-            self.CSVEntry.config(state='readonly')
+            #self.CSVEntry.config(state='readonly')
             return
 
         
         if self.CSVfname:
-            self.CSVEntry.config(state=NORMAL)
+            #self.CSVEntry.config(state=NORMAL)
             self.CSVEntry.delete(0,END)
             self.CSVEntry.insert(0,self.CSVfname)
-            self.CSVEntry.config(state='readonly')
+            #self.CSVEntry.config(state='readonly')
 
     def getDirectory(self):
         self.fDirectory = tkfd.askdirectory()
         if self.fDirectory:
-            self.DEntry.config(state=NORMAL)
+            #self.DEntry.config(state=NORMAL)
             self.DEntry.delete(0,END)
             self.DEntry.insert(0,self.fDirectory)
-            self.DEntry.config(state='readonly')
+            #self.DEntry.config(state='readonly')
 
     def doIt(self):
+        if self.XMLfname == ". . .":
+            tkmb.showwarning("Error", "Error opening XML file.  Please verify file path and extension (.xml only)")
+            return
 
+        if self.CSVfname == ". . .":
+            tkmb.showwarning("Error", "Error opening CSV file.  Please verify file path and extension (.csv only)")
+            return
+
+        if self.fDirectory == ". . .":
+            tkmb.showwarning("Error", "Error accessing File Destination.  Please verify file destination")
+            return
         
         if (tkmb.askyesno("Continue?", "Auto-Generate " + str(len(self.csvdata)-1) + " XML Files from template at \"" + self.fDirectory + "\"?") == False):
             return
+
+        self.browse1.config(state=DISABLED)
+        self.browse2.config(state=DISABLED)
+        self.browse3.config(state=DISABLED)
+        self.cont.config(state=DISABLED)
         
+        
+        self.progressWin()
+        
+        self.timer()
+        self.generate()
+        
+        if not self.isWorking:
+            tkmb.showinfo("Failed", "Something done messed up.  Hopefully this just message just means you cancelled it")
+        
+
+        
+        
+
+    def generate(self):
         fields = self.csvdata[0]
         for x in fields:
             if self.myRoot.find(x) == None:
-                tkmb.showwarning("Error", "CSV fields properly qualified.  Please verify that CSV column headers match XML elements")
+                tkmb.showwarning("Error", "CSV fields not properly qualified.  Please verify that CSV column headers match XML elements")
                 return
 
-        dirname = os.path.join(self.fDirectory,strftime("AutoGen%m-%d-%Y %H.%M.%S",gmtime()))
+        self.dirname = os.path.join(self.fDirectory,strftime("AutoGen%m-%d-%Y %H.%M.%S",gmtime()))
 
-        if not os.path.isdir(dirname):
-            os.mkdir(dirname)
+        if not os.path.isdir(self.dirname):
+            os.mkdir(self.dirname)
 
-        self.rootwin2 = Tk()
-        self.rootwin2.title("Progress")
-        mw2 = self.rootwin2
-
-        self.progress = ttk.Progressbar(mw2, orient="horizontal", length=400, mode="determinate")
-        self.progress.pack()
-
-        time.sleep(1)
-    
-        anum = 0
+        self.isWorking = True
+        self.datalen = len(self.csvdata)
+        anum = 1
         for x in range(len(self.csvdata)-1):
-            print("x" + x)
+            if not self.isWorking:
+                shutil.rmtree(self.dirname)
+                return
             for y in range(len(self.csvdata[x+1])):
-                print(y)
                 self.myRoot.find(self.csvdata[0][y]).text = self.csvdata[x+1][y]
+            self.myTree.write(os.path.join(self.dirname, self.filename + "_" + str(anum) + ".xml"))
+            self.progressLabel.config(text=("Generated "+str(anum+1)+" out of "+str(self.datalen)+" files"))
             anum+=1
-            self.myTree.write(os.path.join(dirname, self.filename + "_" + str(anum) + ".xml"))
-
-        tkmb.showinfo("Success", "Files successfully generated at " + dirname)
-
+            if anum < self.datalen:
+                self.progress.step()
+            self.end_time = time.time()
+            self.timeLabel.config(text=str(datetime.timedelta(seconds=math.ceil((self.end_time - self.start_time)))))
+            self.rootwin2.update()
+        
+        self.progress.config(value="100")
+        
         time.sleep(1)
 
-        self.rootwin2.destroy()
+        tkmb.showinfo("Success", "Files successfully generated at " + self.dirname)
 
-    def progress(self):
+        self.cancel.config(state=DISABLED)
+        self.viewButton.config(state=NORMAL)
+        self.closeProgress.config(state=NORMAL)
+
+        self.rootwin2.lift()
+
+        self.progress.config(value=str(len(self.csvdata)))
+
+
+    def timer(self):
+        self.start_time = time.time()
+
+    def progressWin(self):
+        self.rootwin2 = Toplevel()
+        self.rootwin2.title("Progress")
+        self.rootwin2.protocol("WM_DELETE_WINDOW", self.closeProg)
+        mw2 = self.rootwin2
+        f = Frame(mw2, bd=20)
+        f.pack()
+
+        label = Label(f, text="Generating Files...")
+        label.grid(row=0, columnspan=2, pady=10)
+
+        self.progress = ttk.Progressbar(f, orient="horizontal", length=400, mode="determinate", maximum=len(self.csvdata))
+        self.progress.grid(row=1, columnspan=2)
+
+        self.progressLabel = Label(f, text=("Generated 0 out of "+str(self.datalen)+" files"))
+        self.progressLabel.grid(row=2, column=0)
+
+        self.timeLabel = Label(f, text="00:00")
+        self.timeLabel.grid(row=3, column=0, )
+
+        self.cancel = Button(f, text="Cancel", command=self.breakLoop)
+        self.cancel.grid(row=2, column=1, rowspan=2, pady=10)
+
+        ttk.Separator(f, orient=HORIZONTAL).grid(row=4, columnspan=2, sticky="ew")
+
+        self.viewButton = Button(f, text="View Files", state=DISABLED, command=self.viewFiles)
+        self.viewButton.grid(row=5, column=0, pady=10)
+
+        self.closeProgress = Button(f, text="Close", state=DISABLED, command=self.closeProg)
+        self.closeProgress.grid(row=5, column=1, pady=10)
         
+
+    def viewFiles(self):
+        subprocess.Popen('explorer '+self.dirname)
+
+    def closeProg(self):
+        self.rootwin2.destroy()
+        self.browse1.config(state=NORMAL)
+        self.browse2.config(state=NORMAL)
+        self.browse3.config(state=NORMAL)
+        self.cont.config(state=NORMAL)
+
+    def breakLoop(self):
+        self.isWorking = False
+
+    def test(self):
+        self.XMLfname = os.getcwd()+"/SAMPLE_FILES/SAMPLE_XML.xml"
+        #self.XMLEntry.config(state=NORMAL)
+        self.XMLEntry.delete(0,END)
+        self.XMLEntry.insert(0,self.XMLfname)
+        #self.XMLEntry.config(state='readonly')
+        XMLfile = open(self.XMLfname,'r')
+        self.myTree = et.parse(XMLfile)
+        XMLfile.close()
+        self.myRoot = self.myTree.getroot()
+        self.filename = self.XMLfname[self.XMLfname.rfind('/')+1:len(self.XMLfname)-4]
+
+        self.CSVfname = os.getcwd()+"/SAMPLE_FILES/SAMPLE_CSV.csv"
+        #self.CSVEntry.config(state=NORMAL)
+        self.CSVEntry.delete(0,END)
+        self.CSVEntry.insert(0,self.CSVfname)
+       # self.CSVEntry.config(state='readonly')
+        CSVFile = open(self.CSVfname, 'r')
+        self.csvdata = CSVFile.readlines()
+        CSVFile.close()
+        for x in range(len(self.csvdata)):
+            self.csvdata[x] = self.csvdata[x].split(',')
+            self.csvdata[x][len(self.csvdata[x])-1] = self.csvdata[x][len(self.csvdata[x])-1][:len(self.csvdata[x][len(self.csvdata[x])-1])-1]
+
+        self.fDirectory = os.getcwd()+"\SAMPLE_FILES"
+        #self.DEntry.config(state=NORMAL)
+        self.DEntry.delete(0,END)
+        self.DEntry.insert(0,self.fDirectory)
+        #self.DEntry.config(state='readonly')
         
 ##        if self.isWorking == False:
 ##            self.isWorking = True
@@ -221,14 +345,13 @@ class GUI:
 ##            cont = Button(mw2, text="step", command=self.step)
 ##            cont.pack()
 
-             
-            
+    ####unused
+    def test2(self):
+        threading.Thread.__init__(self)
+
+    ####unused   
     def step(self):
         self.progress.step()
-    
-    def closeProgress(self):
-        self.rootwin2.destroy()
-        self.isWorking = False
 
 
 
